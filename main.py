5b24453836724e676e6dec2071b08bb803e482f6
gk <gk@adamant.local>
2023-05-22 19:23:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+# number = int(input('ввод\n'))
+#
+# if number > 99 and number < 1000:
+#     num1 = number // 100
+#     num2 = number % 100 // 10
+#     num3 = number % 10
+#
+#     if num1 == 6: # if num1 ==6 or
+#         print( ' first number')
+#     elif num2 == 6:
+#         print('second number')
+#     elif num3 == 6:
+#         print('third number')
+#     else:
+#         print('nothing')
+# else:
+#     print('error')
+
+# k= int(input('vvedi \n'))
+# if k >= 1 and k <= 365:
+#     num = k % 7
+#     if num == 0:
+#         print('воскресенье')
+#     elif num == 1:
+#         print('понедельник')
+#     elif num == 2:
+#         print('вторник')
+#     elif  num == 3:
+#         print('среда')
+#     elif num == 4:
+#         print('четверг')
+#     elif  num == 5:
+#         print('пятница')
+#     elif num == 6:
+#         print('суббота выходной')
+#
+# else:
+# #     print('введено не верное число')
+# num = int(input('ввести число\n'))
+# num2 = int(input('ввести число\n'))
+# num3 = int(input('ввести число\n'))
+# num4 = int(input('ввести число\n'))
+# if num > num2:
+#     print(num)
+# elif num2 > num3:
+#     print(num2)
+# elif num3 > num4:
+#     print(num3)
+# elif num > num4:
+#     print(num4)
+# else:
+#     print('большее число')
+#
+
+#4.104
+a = int(input('введите день недели\n'))
+if a == 1:
+    print('понедельник')
+elif a == 2:
+    print('вторник')
+elif a == 3:
+    print('среда')
+elif a == 2:
+    print('четверг')
+elif a == 3:
+    print('пятница')
+elif a == 2:
+    print('суббота')
+elif a == 3:
+    print('воскресенье')
+else:
+    print('error')
+
+## pflkgkghs
